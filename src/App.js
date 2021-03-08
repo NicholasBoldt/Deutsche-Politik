b31860d92ac306c22756960213a7addf714563d3
nicholasboldt <nicholas.boldt@gmail.com>
2021-03-08 14:02:24 -0800

    working on grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Grid } from '@material-ui/core';
+import Leader from './components/Leader/Leader';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container>
+        <Grid item xs={0} sm={2} />
+        <Grid item container xs={12} sm={8} justify='space-between' >
+          <Grid item>
+            <Leader
+              title={"President"}
+              name={"Frank-Walter Steinmeier"}
+              img={
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Frank-Walter_Steinmeier_-_2018_%28cropped%29.jpg/1024px-Frank-Walter_Steinmeier_-_2018_%28cropped%29.jpg"
+              }
+            />
+          </Grid>
+          <Grid item>
+            <Leader
+              title={"Chancellor"}
+              name={"Angela Merkel"}
+              img={
+                "https://upload.wikimedia.org/wikipedia/commons/0/0f/Angela_Merkel_2019_cropped.jpg"
+              }
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={0} sm={2} />
+      </Grid>
     </div>
   );
 }
---
+++ src/components/Leader/Leader.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 500,
+      width: 300
+    },
+  });
+  
+
+function Leader(props) {
+    const classes = useStyles();
+
+    return (
+      <Card className={classes.root}>
+        <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" align="center" component="h2">
+            {props.title}
+          </Typography>
+        </CardContent>
+          <CardMedia
+            className={classes.media}
+            image={props.img}
+            title={props.title + "of Germany"}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" align="center" component="h2">
+              {props.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {props.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+}
+
+export default Leader;
