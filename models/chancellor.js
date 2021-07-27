b7e2149b1bb8c12e30d36327bd720a480ac5268e
nicholasboldt <nicholas.boldt@gmail.com>
2021-07-26 18:38:00 -0700

    start over

--- models/chancellor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-var chancellorSchema = new mongoose.Schema({
-    name: String,
-    party: String,
-});
-
-module.exports = mongoose.model("Chancellor", chancellorSchema);
